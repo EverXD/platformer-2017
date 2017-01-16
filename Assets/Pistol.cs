e8eb738e4cc40ffe0df0467fd730ec0e7eea122c
LEARNET\victor.johansson5 <everxx3@gmail.com>
2017-01-16 16:28:30 +0100

    no message

---
+++ Assets/Pistol.cs
@@ -0,0 +1,23 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class Pistol : MonoBehaviour 
+{
+
+	public float firerate = 0; // hur snabbt din pistol kan skjuta
+	public float damage = 0; //skada av skottet
+
+	float timeToFire = 0; // hur långt det tar för skottet att ta sig ut när du klickar på X
+	Transform firePoint;
+
+	void Awake () 
+	{
+		firePoint = Transform.FindChild ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}*/
---
+++ Assets/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+ public float maxSpeed = 3;
+ public float speed = 50f;
+ public float jumpPower = 10f;
+ private bool secondJumpAvail = true;
+ public bool grounded;
+
+ private Rigidbody2D rb2d;
+
+  void Start () 
+  {
+   rb2d = gameObject.GetComponent<Rigidbody2D>();
+  }
+
+  void Update () 
+  {
+  	if(Input.GetKeyDown(KeyCode.Space))
+  	{
+  		if(grounded == true)
+  		{
+			rb2d.velocity = Vector2.zero;
+			rb2d.AddForce(Vector2.up * jumpPower);
+  			grounded = false;
+  		}
+  		else if(secondJumpAvail == true)
+  		{
+  			rb2d.velocity = Vector2.zero;
+			rb2d.AddForce(Vector2.up * jumpPower);
+  			secondJumpAvail = false;	
+  		}
+  	}
+
+ }
+   void FixedUpdate()
+  { // få spelaren att röra sig
+
+   if(Input.GetKey(KeyCode.A) == false)
+   {
+   rb2d.velocity = new Vector2 (maxSpeed * 0, rb2d.velocity.y);
+   }
+
+		if(Input.GetKey(KeyCode.D) == false)
+   {
+   rb2d.velocity = new Vector2 (maxSpeed * 0, rb2d.velocity.y);
+   }
+
+   float h = Input.GetAxis("Horizontal");
+
+   rb2d.AddForce((Vector2.right * speed) * h);
+    // Sätt en maxspeed 
+   if(rb2d.velocity.x > maxSpeed)
+   {
+    rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+   }
+   if(rb2d.velocity.x < -maxSpeed)
+   {
+    rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+   }
+  }
+  void OnCollisionEnter2D(Collision2D coll)
+  {
+  	if(coll.gameObject.tag == "Ground")
+  	{
+  		grounded = true;
+		secondJumpAvail = true;
+  	}
+	if(coll.gameObject.tag == "Destroy")
+	{
+		Destroy(gameObject);
+	}
+  }
+
+  }
